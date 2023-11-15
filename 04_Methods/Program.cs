fa347da5e9196f555cbbed1963d8125436ec25df
1mirmhmd <mirmhmdmahmud@gmail.com>
2023-11-16 01:04:20 +0300

    Methods project added

---
+++ 04_Methods/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _04_Methods
+{
+    // Dont Repeat Yourself - DRY
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
