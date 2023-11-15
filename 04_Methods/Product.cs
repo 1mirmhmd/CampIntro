ed02f082db1dc63099732f010997db91db7a5707
1mirmhmd <mirmhmdmahmud@gmail.com>
2023-11-16 01:12:51 +0300

    Method's additional classes added

---
+++ 04_Methods/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Methods
+{
+    internal class Product
+    {
+        // Property -> Özellik
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public int UnitInStock { get; set; }
+    }
+}
--- 04_Methods/obj/Debug/net6.0/04_Methods.AssemblyInfo.cs
+++ 04_Methods/obj/Debug/net6.0/04_Methods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04_Methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa347da5e9196f555cbbed1963d8125436ec25df")]
 [assembly: System.Reflection.AssemblyProductAttribute("04_Methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04_Methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
